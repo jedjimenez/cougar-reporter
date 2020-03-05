09cdae7fe8929f627c91ff869ba8e123e54f56ae
Michael <tobin012@cougars.csusm.edu>
2020-03-05 09:59:11 -0800

    INIT

---
+++ cougar_reporter/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using cougar_reporter.Services;
+using cougar_reporter.Views;
+
+namespace cougar_reporter
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
