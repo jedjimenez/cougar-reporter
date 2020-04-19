427b352290a45268d5dc72ac737111d61df71c0e
monte0 <47285725+monte0@users.noreply.github.com>
2020-04-18 19:20:50 -0700

    submitReport0 = tabbed page

---
+++ cougar_reporter/Views/SubmitReport0.xaml.cs
@@ -0,0 +1,153 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cougar_reporter.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubmitReport0 : TabbedPage
+    {
+        public string Rpick = "<Repair Type>";
+        public string Bpick = "<Building>";
+        public string Entry1 = "<Room>";
+        public string Editor1 = "....";
+        public SubmitReport0()
+        {
+            InitializeComponent();
+            //add possible choices to pick
+            RepairPicker.Items.Add("Type 1");
+            RepairPicker.Items.Add("Type 2");
+            RepairPicker.Items.Add("Type 3");
+            RepairPicker.Items.Add("Type 4");
+            BuildingPicker.Items.Add("Building 1");
+            BuildingPicker.Items.Add("Building 2");
+            BuildingPicker.Items.Add("Building 3");
+            BuildingPicker.Items.Add("Building 4");
+        }
+
+        private void RepairPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //save user pick into variable
+            string name = RepairPicker.Items[RepairPicker.SelectedIndex];
+            // App.Current.MainPage.DisplayAlert(name, "Selected", "OK");
+            //this.Rpick = name;
+            RepairText.Text = name;
+        }
+
+        private void BuildingPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //save user pick into variable
+            var name = BuildingPicker.Items[BuildingPicker.SelectedIndex];
+            //App.Current.MainPage.DisplayAlert(name, "Selected", "OK");
+            //this.Bpick = name;
+            BuildingText.Text = name;
+        }
+
+        private void RoomNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //change old text into new text
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        private void RoomNumber_Completed(object sender, EventArgs e)
+        {
+            //all entries has been picked
+            string text = ((Entry)sender).Text;
+            //this.Entry1 = text;
+            RoomText.Text = text;
+        }
+
+        private void UserText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        private void UserText_Completed(object sender, EventArgs e)
+        {
+            string text = ((Editor)sender).Text;
+            //this.Editor1 = text;
+            Description.Text = text;
+        }
+
+        private async void FolderButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                //all possbile case errors
+                await DisplayAlert("Oops", "Pick Photo is not supported !", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+
+            if (file == null)
+            {
+                return;
+            }
+            //Photo path
+            PathLabel.Text = "Photo Path" + file.Path;
+
+            MainImage.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            //check for errors open camera
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No camera", ":( No camera available.", "OK");
+                return;
+            }
+            else
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(
+                //Save into local file
+                new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    Directory = "Sample",
+                    Name = "Test.jpg"
+                });
+
+                if (file == null)
+                {
+                    return;
+                }
+
+                PathLabel.Text = file.AlbumPath;
+
+                MainImage.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            }
+        }
+
+        private async void SubmitButton_Clicked(object sender, EventArgs e)
+        {
+            //open content page with the user's vlaues
+            //await Navigation.PushAsync(new ContentPage(_repair, _building, _room, _des));
+            await Navigation.PushAsync(new ContentPage());
+        }
+    }
+}
