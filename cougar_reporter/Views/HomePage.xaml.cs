a43b5717961fd17e23a0c1578773a01bace9f4f3
edwin502 <43422710+edwin502@users.noreply.github.com>
2020-05-13 23:12:57 -0700

    Update HomePage.xaml.cs
    added functionality to the buttons

--- cougar_reporter/Views/HomePage.xaml.cs
+++ cougar_reporter/Views/HomePage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,12 +13,51 @@ namespace cougar_reporter.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
-        
         public HomePage(string u)
         {
             InitializeComponent();
             
         }
 
+        private void Previous(object sender, EventArgs e)
+        {
+            int count = 0;
+
+            if (count == 0)
+            {
+                building.Text = "Academic Hall";
+                room.Text = "206";
+                date.Text = "5/14/2020";
+                count++;
+            }
+
+            else
+            {
+                building.Text = "Science Hall II";
+                room.Text = "420";
+                date.Text = "4/20/20";
+                count++;
+            }
+        }
+        private void Next(object sender, EventArgs e)
+        {
+            int count = 0;
+
+            if (count == 0)
+            {
+                building.Text = "Science Hall II";
+                room.Text = "235";
+                date.Text = "5/14/2020";
+                count++;
+            }
+
+            else
+            {
+                building.Text = "University Hall";
+                room.Text = "311";
+                date.Text = "5/14/2020";
+                count++;
+            }
+        }
     }
 }
